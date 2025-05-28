7712984d0eb891529994aa9cc4ff4e74b0a9df81
Artem <hamoart722@gmail.com>
2025-05-28 17:50:54 +0300

    Реалізував виведення Listings та показ одного елемента. Також реалізував додавання і обробку збереження даних

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,14 +1,31 @@
 <template>
     <Link href="/">Main Page</Link>&nbsp;
     <Link href="/hello">Show Page</Link>
-    <div>The page with time {{ timer }}</div>
+    <!-- <div>The page with time {{ timer }}</div> -->
+    <div v-if="flashSuccess" class="success">
+        {{ flashSuccess }}
+    </div>
     <slot />
 </template>
 
 <script setup>
-import { Link } from '@inertiajs/vue3'
-import { ref } from 'vue'
+import { Link, usePage } from '@inertiajs/vue3'
+import { computed } from 'vue'
 
-const timer = ref(0)
-setInterval(() => timer.value++, 1000)
+const page = usePage()
+const flashSuccess = computed(
+  () => page.props.flash.success,
+)
+//import { ref } from 'vue'
+
+// const timer = ref(0)
+// setInterval(() => timer.value++, 1000)
 </script>
+
+
+<style scoped>
+    .success{
+        background-color: green;
+        color: white;
+    }
+</style>
