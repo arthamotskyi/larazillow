d63aff6adc7b597d72436db1039f0e3cf0956c9e
Artem <hamoart722@gmail.com>
2025-05-30 21:39:31 +0300

    Зробив стилізацію для форм Create і Edit

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -10,7 +10,7 @@
                 </div>
                 <div>
                     <Link :href="route('listing.create')"
-                        class="bg-indigo-600 hover:bg-indigo-500 text-white font-medium p-2 rounded-md">+ New Listing</Link>
+                        class="btn-primary">+ New Listing</Link>
                 </div>
             </nav>
         </div>
