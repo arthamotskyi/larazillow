4f2e2bedb7f7dfb3d1714c007c33e99cd218a87d
Artem <hamoart722@gmail.com>
2025-06-03 20:12:29 +0300

    Створив функціонал входу і реєстрації нових користувачів у системі

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -8,15 +8,29 @@
                 <div class="text-xl text-indigo-600 dark:text-indigo-300 font-bold text-center">
                     <Link :href="route('listing.index')">LaraZillow</Link>
                 </div>
-                <div>
+                <div v-if="user" class="flex items-center gap-4">
+                    <div class="text-sm text-gray-500">
+                        {{ user.name }}
+                    </div>
                     <Link :href="route('listing.create')"
                         class="btn-primary">+ New Listing</Link>
+                    <div>
+                        <Link :href="route('logout')" method="DELETE" as="button" class="cursor-pointer">
+                            Logout
+                        </Link>
+                    </div>
+                </div>
+                <div v-else class="flex items-center gap-2">
+                    <Link :href="route('user-account.create')">
+                        Register
+                    </Link>
+                    <Link :href="route('login')">Sign-In</Link>
                 </div>
             </nav>
         </div>
     </header>
 
-    <main class="container mx-auto p-4">
+    <main class="container mx-auto p-4 w-full">
         <div v-if="flashSuccess"
             class="mb-4 border rounded-md shadow-sm border-green-200 dark:border-green-800 bg-green-50 dark:bg-green-900 p-2">
             {{ flashSuccess }}
@@ -34,5 +48,9 @@ import { route } from 'ziggy'
 const page = usePage()
 const flashSuccess = computed(
   () => page.props.flash.success,
+)
+
+const user = computed(
+  () => page.props.user,
 )
 </script>
