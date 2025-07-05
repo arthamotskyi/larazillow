f725bcd2539a363c27964b50075abfa38cf9380d
Artem <hamoart722@gmail.com>
2025-07-05 20:27:11 +0300

    Зробив функціонал відправки повідомлень користувачам, на чиї оголошення зробили пропозицію

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -9,6 +9,14 @@
                     <Link :href="route('listing.index')">LaraZillow</Link>
                 </div>
                 <div v-if="user" class="flex items-center gap-4">
+                    <Link class="text-gray-500 relative pr-2 py-2 text-lg"
+                        :href="route('notification.index')">
+                        🔔
+                        <div v-if="notificationCount" class="absolute right-0 top-0 w-5 h-5 bg-red-700 dark:bg-red-400 text-white font-medium border border-white dark:border-gray-900 rounded-full text-xs text-center">
+                            {{ notificationCount }}
+                        </div>
+                    </Link>
+
                     <Link :href="route('realtor.listing.index')" class="text-sm text-gray-500">
                         {{ user.name }}
                     </Link>
@@ -52,5 +60,9 @@ const flashSuccess = computed(
 
 const user = computed(
   () => page.props.user,
+)
+
+const notificationCount = computed(
+  () => Math.min(page.props.user.notificationCount, 9),
 )
 </script>
--- resources/js/Pages/Realtor/Index.vue
+++ resources/js/Pages/Realtor/Index.vue
@@ -3,7 +3,7 @@
     <section>
         <RealtorFilters :filters="filters"/>
     </section>
-    <section class="grid grid-cols-1 lg:grid-cols-2 gap-2">
+    <section v-if="listings.data.length" class="grid grid-cols-1 lg:grid-cols-2 gap-2">
         <Box v-for="listing in listings.data" :key="listing.id" :class="{'border-dashed': listing.deleted_at}">
             <div class="flex flex-col md:flex-row gap-2 md:items-center justify-between">
                 <div :class="{'opacity-25': listing.deleted_at}">
@@ -58,6 +58,7 @@
             </div>
         </Box>
     </section>
+    <EmptyState v-else>No listing yet</EmptyState>
 
     <section v-if="listings.data.length" class="w-full flex justify-center mt-4 mb-4">
         <Pagination :links="listings.links"/>
@@ -74,6 +75,7 @@ import { Link } from '@inertiajs/vue3'
 import { route } from 'ziggy'
 import RealtorFilters from '@/Pages/Realtor/Index/Components/RealtorFilters.vue'
 import Pagination from '@/Components/UI/Pagination.vue'
+import EmptyState from '@/Components/UI/EmptyState.vue'
 
 defineProps({
   listings: Object,
