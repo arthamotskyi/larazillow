1c02ab3ba9c8f5472cadca39e9323f26ee3738ee
Artem <hamoart722@gmail.com>
2025-07-06 22:05:35 +0300

    Зробив функціонал підтверження акаунту, після його створення, та можливість надіслати повідомлення для верифікації ще раз

---
+++ resources/js/Pages/Auth/VerifyEmail.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        You need to verified first to see this page!
+    </div>
+    <div>
+        <Link :href="route('verification.send')" method="post" as="button" class="text-indigo-600 dark:text-indigo-400 cursor-pointer">
+            Resend verification email
+        </Link>
+    </div>
+</template>
+
+<script setup>
+import { Link } from '@inertiajs/vue3'
+import { route } from 'ziggy'
+</script>
